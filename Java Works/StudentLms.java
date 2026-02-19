1704a07ca6962e9c1b672477b1293af080bfc608
varun4747 <varunpapanaboina4747@gmail.com>
2026-02-19 20:34:00 +0530

    dataTypes and Variable Created

---
+++ Java Works/StudentLms.java
@@ -0,0 +1,17 @@
+public class StudentLms{
+    public static void main(String[] args) {
+        String studentId="1";
+        String studentName="John Doe";
+        String studentEmail="jhon@gmail.com";
+        String studentPassword="Java Programming";
+        long studentMobile=1234567890L;
+        boolean isStudentMale=true;
+        System.out.println("Student Id: "+studentId);
+        System.out.println("Student Name: "+studentName);
+        System.out.println("Student Email: "+studentEmail);
+        System.out.println("Student Password: "+studentPassword);
+        System.out.println("Student Mobile: "+studentMobile);
+        System.out.println("Is Student Male: "+isStudentMale);
+
+    }
+}
