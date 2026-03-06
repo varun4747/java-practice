8ed8a566f2b7a62f2597a525a05df3e2ef1c0d78
varun4747 <varunpapanaboina4747@gmail.com>
2026-03-06 22:08:42 +0530

    try catch created

--- JavaWorks/CheckingVoters.java
+++ JavaWorks/CheckingVoters.java
@@ -3,20 +3,27 @@ import java.util.Scanner;
 public class CheckingVoters {
 
     public void votersChecking() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your Name:");
-        String nameResult = sc.nextLine();
-        System.out.println("enter your age:");
-        int result = sc.nextInt();
 
-        if (result < 18) {
-            System.out.println(nameResult + " " + "you are not eligible to vote");
-        } else if (result > 18) {
-            System.out.println(nameResult + " " + "you are  eligible to vote");
-        } else {
-            System.out.println(nameResult + " " + "just one year ahead");
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter your Name:");
+            String nameResult = sc.nextLine();
+            System.out.println("enter your age:");
+            int result = sc.nextInt();
+
+            if (result < 18) {
+                System.out.println(nameResult + " " + "you are not eligible to vote");
+            } else if (result > 18) {
+                System.out.println(nameResult + " " + "you are  eligible to vote");
+            } else {
+                System.out.println(nameResult + " " + "just one year ahead");
+            }
+            sc.close();
+
+        } catch (Exception e) {
+            System.err.println(e);
         }
-        sc.close();
+
     }
 
     public static void main(String[] args) {
