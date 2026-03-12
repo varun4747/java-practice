53379f5efce7291e6652ecf339855b9a4566e9cc
varun4747 <varunpapanaboina4747@gmail.com>
2026-03-12 19:47:02 +0530

    Is-a relationship [inheritance] folder and file created and done inheritance

---
+++ JavaWorks/inheritance/InheritanceClass.java
@@ -0,0 +1,29 @@
+package inheritance;
+
+class p {
+    public void m1(int a,int b){
+        System.out.println(a+b);
+    }
+    public void m2(int c, int d){
+        System.out.println(c-d);
+    }
+}
+class c extends p{
+    public void m3(int e,int f){
+        System.out.println(e*f);
+    }
+    public void m4(int g, int h){
+        System.out.println(g/h);
+    }
+
+}
+
+public class InheritanceClass{
+    public static void main(String[] args) {
+        p incObj=new p();
+        p incObj1=new c ();
+        incObj1.m1(10,20);
+        incObj1.m2(20,10);
+        incObj1.m3();
+    }
+}
