2c94c4e3c36c4a2f43242f1a3cf41a9081b4289f
varun4747 <varunpapanaboina4747@gmail.com>
2026-03-15 21:13:45 +0530

    folder and file created for has a relationship

---
+++ JavaWorks/HasArealtionship/hasARelation.java
@@ -0,0 +1,15 @@
+package HasArealtionship;
+
+class Engine {
+public void engineMethod(){
+    System.out.println("Engine created");
+}    
+}
+
+
+class Driver{
+    public void carDriver(){
+        Engine engine=new Engine();
+        System.out.println
+    }
+}
